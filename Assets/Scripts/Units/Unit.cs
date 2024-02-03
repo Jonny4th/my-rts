cf70d369551906d13155ed1bfa845e4236b45e52
Kajornpop Toboonchuay <jonny4th@gmail.com>
2024-02-03 11:05:06 +0700

    Set up unit movement functions

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -73,5 +73,34 @@ namespace MyGame.Unit
         {
 
         }
+
+        public void SetState(UnitState toState)
+        {
+            state = toState;
+
+            if (state == UnitState.Idle)
+            {
+                navAgent.isStopped = true;
+                navAgent.ResetPath();
+            }
+        }
+        public void MoveToPosition(Vector3 dest)
+        {
+            if (navAgent != null)
+            {
+                navAgent.SetDestination(dest);
+                navAgent.isStopped = false;
+            }
+
+            SetState(UnitState.Move);
+        }
+
+        private void MoveUpdate()
+        {
+            float distance = Vector3.Distance(transform.position, navAgent.destination);
+
+            if (distance <= 1f)
+                SetState(UnitState.Idle);
+        }
     }
 }
