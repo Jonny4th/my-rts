11d1956862f31ed308377620ea6d9612ac73c2db
Kajornpop Toboonchuay <jonny4th@gmail.com>
2024-02-03 14:25:13 +0700

    set up head quarter

--- Assets/Scripts/Buildings/Structure.cs
+++ Assets/Scripts/Buildings/Structure.cs
@@ -4,6 +4,26 @@ namespace MyGame.Core
 {
     public class Structure : MonoBehaviour
     {
+        [SerializeField]
+        protected string structureName;
+        public string StructureName { get { return structureName; } }
 
+        [SerializeField] protected Sprite structurePic;
+        public Sprite StructurePic { get { return structurePic; } }
+
+        [SerializeField]
+        protected int curHP;
+        public int CurHP { get { return curHP; } set { curHP = value; } }
+
+        [SerializeField]
+        protected int maxHP;
+        public int MaxHP { get { return maxHP; } set { maxHP = value; } }
+
+        [SerializeField]
+        protected Faction faction;
+
+        [SerializeField]
+        protected GameObject selectionVisual;
+        public GameObject SelectionVisual { get { return selectionVisual; } }
     }
 }
