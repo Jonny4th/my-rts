3d91d607bbb3bb29606859aa13ed6a453f42d10a
Tong <91357144+Jonny4th@users.noreply.github.com>
2024-02-24 10:36:24 +0700

    tree

---
+++ Assets/Scripts/Resources/ResourceManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] woodTreePrefab;
+
+    [SerializeField]
+    private Transform woodTreeParent;
+
+    [SerializeField]
+    private ResourceSource[] resources;
+
+    public static ResourceManager instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            return;
+        }
+
+        Destroy(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindAllResource();
+    }
+
+    private void FindAllResource()
+    {
+        resources = FindObjectsOfType<ResourceSource>();
+    }
+}
