9dd24f3f8b3745f4387c2ec086a33d97e97bdd97
Kajornpop Toboonchuay <jonny4th@gmail.com>
2024-03-09 13:07:32 +0700

    test ui

---
+++ Assets/Scripts/UI/ScrollablePanelSize.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ScrollablePanelSize : MonoBehaviour
+{
+    public Transform Min;
+    public Transform Max;
+
+    public AnimationCurve ScaleCurve;
+
+    private void Start()
+    {
+        GetPos();
+    }
+    private void Update()
+    {
+        //UpdateScale();
+    }
+
+    public void UpdateScale()
+    {
+        var point = Mathf.InverseLerp(Max.localPosition.x, Max.position.x, transform.position.x);
+        transform.localScale = ScaleCurve.Evaluate(point) * Vector3.one;
+    }
+
+    public void GetPos()
+    {
+        var point = Mathf.InverseLerp(Max.localPosition.x, Max.localPosition.x, transform.localPosition.x);
+        var eva = ScaleCurve.Evaluate(point);
+        Debug.Log($"{point} / {eva}");
+    }
+}
+
